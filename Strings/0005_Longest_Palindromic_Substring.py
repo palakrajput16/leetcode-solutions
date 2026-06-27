2ecd3ad37d1e0f55cbebdec55dcdb034f694d7bb
palakrajput16 <palakrajput2006@gmail.com>
2026-06-27 23:01:09 +0530

    Add longest palindrome solution

---
+++ Strings/0005_Longest_Palindromic_Substring.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) <= 1:
+            return s
+
+        start = 0
+        end = 0
+
+        for i in range(len(s)):
+            #odd length palindrome
+            len1 = self.expand(s, i , i)
+
+            #even lenght palindrome
+            len2 = self.expand(s, i, i+1)
+
+            max_len = max(len1, len2)
+
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+        return s[start:end + 1]
+
+    def expand(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+
+        return right - left - 1
+        
